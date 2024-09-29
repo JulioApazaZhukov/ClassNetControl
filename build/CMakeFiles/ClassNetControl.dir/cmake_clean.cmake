d77d1373a0dec6304347cf32140f48f30e4b0e18
Julio Apaza <juliovladimiraz@gmail.com>
2024-09-28 20:48:12 -0500

    09-28-2024

---
+++ build/CMakeFiles/ClassNetControl.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassNetControl.dir/src/main.cpp.obj"
+  "CMakeFiles/ClassNetControl.dir/src/main.cpp.obj.d"
+  "ClassNetControl.exe"
+  "ClassNetControl.exe.manifest"
+  "ClassNetControl.pdb"
+  "libClassNetControl.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassNetControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
